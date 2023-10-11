d1a7bf5782c56ba4f46912bc207e428a55f7a7d9
zheng.wang <zheng.Wang@mobiledrivetech.com>
2023-10-11 11:39:31 +0800

    remove some file headers

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Robben Wadlay
+ * @Date: 2023-10-09 17:18:51
+ * @Description: rollup config file
+ * @LastEditors: Robben Wadlay
+ * @LastEditTime: 2023-10-11 11:35:44
+ */
 import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'temp/index.js',
